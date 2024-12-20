58a36359d19678afbe4700ae42d4f26fdcedc925
marcitt <marcitalbot23@gmail.com>
2024-12-20 18:06:35 +0000

    Created live plots with streamlit

---
+++ st_attention_live_plot.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from PIL import ImageGrab
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px  # interactive charts
+import pandas as pd
+import random
+import time
+import os
+import datetime
+import cv2
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+st.title("Dashboard")
+
+from detect_attention import measure_attention
+
+attention_timestamps = []
+attention_readings = []
+
+attention_placeholder = st.empty()
+
+for seconds in range(100):
+    df_attention = pd.DataFrame(dict(x=attention_timestamps, y=attention_readings))
+    fig_attention = px.line(df_attention, x="x", y="y", title="Attention Level")
+    attention_placeholder.plotly_chart(
+        fig_attention, key=f"attention-{seconds}"
+    )
+
+    attention_timestamps.append(datetime.datetime.now())
+    attention_readings.append(measure_attention())
+
+
---
+++ st_live_plots.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from PIL import ImageGrab
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px  # interactive charts
+import pandas as pd
+import random
+import time
+import os
+import datetime
+import cv2
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+st.title("Dashboard")
+
+from detect_attention import measure_attention
+from detect_screen_text import detect_text
+
+text_timestamps = []
+text_readings = []
+
+attention_timestamps = []
+attention_readings = []
+
+attention_placeholder = st.empty()
+text_placeholder = st.empty()
+
+for seconds in range(100):
+    df_attention = pd.DataFrame(dict(x=attention_timestamps, y=attention_readings))
+    fig_attention = px.line(df_attention, x="x", y="y", title="Attention Level")
+    attention_placeholder.plotly_chart(fig_attention, key=f"attention-{seconds}")
+
+    attention_timestamps.append(datetime.datetime.now())
+    attention_readings.append(measure_attention())
+
+    # text plot
+    df_text = pd.DataFrame(dict(x=text_timestamps, y=text_readings))
+
+    fig_text = px.line(df_text, x="x", y="y", title="Text Patterns")
+    text_placeholder.plotly_chart(fig_text, key=f"text-{seconds}")
+
+    text_timestamps.append(datetime.datetime.now())
+    text_readings.append(detect_text())
